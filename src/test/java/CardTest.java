6d016c304e94771c4e56983da1af093043ae6caa
Екатерина <laufeyconyy@gmail.com>
2021-10-28 21:12:49 +0300

    finally2

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -15,7 +15,7 @@ public class CardTest {
 
     @BeforeAll
     static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
+        //System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
         WebDriverManager.chromedriver().setup();
     }
 
